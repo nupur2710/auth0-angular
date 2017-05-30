1083898f4dd0e05ed5a33a17ea0064beec5a7cc6
Nupur <nupur@Admins-Mac-mini.local>
2017-05-30 12:57:09 +0530

    first commit

---
+++ components/profile/profile.ctr.js
@@ -0,0 +1,12 @@
+(function() {
+    'use-strict';
+
+    angular
+        .module('authApp')
+        .controller('profileController', profileController);
+
+    function profileController($http){
+    	var vm = this;
+    	vm.message = "hello";
+    }
+})();
---
+++ components/toolbar/toolbar.dir.js
@@ -0,0 +1,20 @@
+(function() {
+    'use-strict';
+
+    angular
+        .module('authApp')
+        .directive('toolbar', toolbar);
+
+    function toolbar() {
+        return {
+            templateUrl: 'components/toolbar/toolbar.tpl.html',
+            controller: toolbarController,
+            controllerAs: 'toolbar'
+
+        }
+    }
+
+    function toolbarController() {
+
+    }
+})();
