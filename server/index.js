fe25a00f98e0aef658bf9bbc61c2ed8b4679954d
Nupur <nupur@Admins-Mac-mini.local>
2017-05-30 15:04:56 +0530

    node js side functionality

---
+++ server/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var jwt = require('express-jwt');
+
+var cors = require('cors');
+
+app.use(cors());
+
+//setup middleware
+
+var authCheck = jwt({
+    secret: new Buffer('mVXv_k7o4kIa73Cd3n5CW2riBpBIzQ9J1kJ6eXKNE1HOyTaHoSQh6YpjyxyqyAWG', 'base64'),
+    audience: '5BOPjc1TzhEDdyylzwvGL1Uek7q7Bdc9'
+});
+
+//endpoints
+
+//public
+app.get('/api/public', function(req, res) {
+    res.json({ message: "Hello from a public endpoint. No authentication needed" });
+});
+
+app.get('/api/private', authCheck, function(req, res) {
+    res.json({ message: "Hello from a private endpoint.Authentication needed" });
+});
+
+app.listen(3001);
+console.log("listening on localhost 3001");
