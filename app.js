e50b3008a8ba2af21010d9fc4ee10518f523b7b4
Nupur <nupur@Admins-Mac-mini.local>
2017-05-30 16:06:46 +0530

    add login logout functionality - get token

--- app.js
+++ app.js
@@ -3,6 +3,12 @@
 angular
     .module('authApp', ['auth0', 'angular-storage', 'angular-jwt', 'ngMaterial', 'ui.router'])
     .config(function($provide, authProvider, $urlRouterProvider, $stateProvider, $httpProvider, jwtInterceptorProvider, $locationProvider) {
+
+        authProvider.init({
+            domain: "angularjs4.auth0.com",
+            clientID: "5BOPjc1TzhEDdyylzwvGL1Uek7q7Bdc9"
+        })
+        
         $urlRouterProvider.otherwise('/home');
 
         $stateProvider
@@ -16,5 +22,5 @@ angular
                 controller: 'profileController as user'
             });
 
-        $locationProvider.html5Mode(false).hashPrefix("");
+        $locationProvider.html5Mode(true).hashPrefix("");
     })
--- components/toolbar/toolbar.dir.js
+++ components/toolbar/toolbar.dir.js
@@ -14,7 +14,31 @@
         }
     }
 
-    function toolbarController() {
 
+
+    function toolbarController(auth, store, $location) {
+        var vm = this;
+        vm.login = login;
+        vm.logout = logout;
+        vm.auth = auth;
+
+        function login() {
+            auth.signin({}, function(profile, token) {
+                store.set('profile', profile);
+                store.set('id_token', token);
+                //redirect to the path u want
+                $location.path('/home');
+
+            }, function(error) {
+                console.log(error);
+            });
+        }
+
+        function logout() {
+            store.remove('profile');
+            store.remove('id_token');
+            auth.signout();
+            $location.path('/home');
+        }
     }
 })();
